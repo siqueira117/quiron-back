5df4e994a52aa51dc68e66c472a8a7a581c114cb
Matheus Curvelo <matheus_curvelo@hotmail.com>
2024-12-15 23:43:56 -0300

    fix: model entregadores

--- app/Models/Entregador.php
+++ app/Models/Entregador.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Entregador extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tbl_entregadores';
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['nome', 'tel_celular'];
+
+    public function rules() {
+        return [ 'nome' => 'required|max:200', 'tel_celular' => 'required|size:11' ];
+    }
+
+    protected $hidden = [
+        "created_at", "updated_at", "deleted_at"
+    ];
 }
